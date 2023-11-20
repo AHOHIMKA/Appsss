f284b4cf04e2e138e4a7526c3227ab361365742c
AHOHIM <AHOHIM@DESKTOP-MDL0KP2>
2023-11-20 21:40:40 +0300

    Главная странциа +- кривая и меню выбора

--- Appsss/Appsss/views/Home.xaml.cs
+++ Appsss/Appsss/views/Home.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +17,8 @@ namespace Appsss.views
 		public Home ()
 		{
 			InitializeComponent ();
-		}
+
+            
+        }
 	}
 }
